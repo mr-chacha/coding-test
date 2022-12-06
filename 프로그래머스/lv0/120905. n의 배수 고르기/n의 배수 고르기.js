17ccb1ff06b9203e3060d5a60b61afc08c22f66e
차차 <117566375+mr-chacha@users.noreply.github.com>
2022-12-06 09:37:04 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,1 +1,11 @@
-const solution = (n, numlist) => numlist.filter(a => a%n === 0)
+function solution(n, numlist) {
+    var answer = [];
+    for( let i=0; i<numlist.length; i++){
+        if(numlist[i]%n==0){
+            answer.push(numlist[i])
+        }
+    }
+    return answer;
+}
+
+
