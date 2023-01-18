9ca77674e480b9120c367d20b5b0c7b007614626
차차 <117566375+mr-chacha@users.noreply.github.com>
2023-01-18 17:52:47 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,13 @@
+function solution(angle) {
+    if(angle <90){
+        return 1
+    }if(angle==90){
+        return 2
+    }if(angle<180){
+        return 3
+    }if(angle==180){
+        return 4
+    }
+    var answer = 0;
+    return answer;
+}
